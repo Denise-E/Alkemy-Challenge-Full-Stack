ccb119d7d5294727e3ccefe1e2f283f8ef282a29
Denise Eichenblat <93154485+Denise-E@users.noreply.github.com>
2022-04-11 10:12:33 -0300

    Creando modelo con ORM Sequelize

---
+++ database/models/Operation.js
@@ -0,0 +1,34 @@
+module.exports = (Sequelize, DataTypes) => {
+    const alias = "Operation";
+
+    const cols = {
+        id: {
+            primaryKey: true,
+            autoIncrement: true,
+            type: DataTypes.INTEGER
+        }, 
+        type: {
+            notNull: true,
+            type: DataTypes.TEXT,
+        },
+        description: {
+            notNull: true,
+            type: DataTypes.TEXT,
+        },
+        value: {
+            notNull: true,
+            type: DataTypes.TEXT,
+        },
+        date: {
+            notNull: true,
+            type: DataTypes.TEXT,
+        }
+    }
+
+    const config = {
+        timestamps: true,
+    }
+
+    const Operation = Sequelize.define(alias,cols,config);
+    return Operation
+}
