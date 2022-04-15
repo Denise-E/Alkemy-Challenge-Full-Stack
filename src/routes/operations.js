27cb2bd94858b091295b64ca958d8d365716bd0c
Denise Eichenblat <93154485+Denise-E@users.noreply.github.com>
2022-04-15 16:12:59 -0300

    agregando fontawesome, haciendo ruta para editar dinámica y permitiendo su ingreso desde vista de la operación

--- src/routes/operations.js
+++ src/routes/operations.js
@@ -3,11 +3,12 @@ const router = express.Router();
 const operations = require('../controllers/operations.js');
 
 router.get('/create', operations.create);
-router.get('/edit', operations.edit);
+
 router.get('/:id', operations.show);
+router.get('/edit/:id', operations.edit);
 
 router.post('/save', operations.save);
-router.post('/modify', operations.modify);
+//router.post('/modify', operations.modify);
 
 
 module.exports = router;
