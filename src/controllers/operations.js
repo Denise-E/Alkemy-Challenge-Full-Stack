38ce247f54f196bfc7c8cf59dc0dbc75bca5dc34
Denise Eichenblat <93154485+Denise-E@users.noreply.github.com>
2022-04-15 16:08:31 -0300

    creando ruta dinámica de las operaciones

--- src/controllers/operations.js
+++ src/controllers/operations.js
@@ -1,3 +1,4 @@
+const { where } = require("sequelize");
 const sequelize = require("sequelize");
 const db = require('../../database/models');
 
@@ -8,6 +9,14 @@ module.exports = {
     edit: (req,res) =>res.render('edit',{
         style: 'edit'
     }),
+    show: (req,res) => {
+        db.Operation.findByPk(req.params.id)
+        .then((result) =>res.render('show',{
+            operation: result,
+            style: 'show'
+        }))
+        .catch((err) => res.send(err));
+    },
     save: (req,res) => {
       db.Operation.create({
             type: req.body.operationType,
@@ -17,5 +26,15 @@ module.exports = {
         })
         .then(() => res.redirect("/home")) //AJUSTAR DESP A VISTA DINÁMICA DE LA OPERACIÓN
         .catch(err => res.send(err))
-    }
+    },
+    /*
+    modify: (req, res) => {
+        db.Operation.update(
+            {
+                description: req.body.description,
+                value: req.body.total,
+                date: req.body.date
+            }
+            ,{where: req.params.id})
+    }*/
 }
--- src/routes/operations.js
+++ src/routes/operations.js
@@ -4,8 +4,10 @@ const operations = require('../controllers/operations.js');
 
 router.get('/create', operations.create);
 router.get('/edit', operations.edit);
+router.get('/:id', operations.show);
 
 router.post('/save', operations.save);
+router.post('/modify', operations.modify);
 
 
 module.exports = router;
