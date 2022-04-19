f1990b3394c4f2276e3af976b55f6a524b981757
Denise Eichenblat <93154485+Denise-E@users.noreply.github.com>
2022-04-19 10:28:19 -0300

    empezando a mostrar los registros en la home

--- src/controllers/main.js
+++ src/controllers/main.js
@@ -1,6 +1,16 @@
+const sequelize = require('sequelize');
+const db = require('../../database/models');
+const op = sequelize.Op;
 
 module.exports = {
-    index: (req,res) => res.render('home',{
-        style: 'home'
-    })
+    index: (req,res) =>{ 
+        db.Operation.findAll({order: op.DESC, limit: 10})
+        .then((result) =>{
+            res.render('home',{
+                operations: result,
+                style: 'home'
+            })
+        })    
+    }
+
 }
