7d4fc5c61340218f161c22b47f14dfae8c4834d9
Denise Eichenblat <93154485+Denise-E@users.noreply.github.com>
2022-04-22 14:31:21 -0300

    mostrando ultimas 10 operaciones en la home

--- src/controllers/main.js
+++ src/controllers/main.js
@@ -4,13 +4,14 @@ const op = sequelize.Op;
 
 module.exports = {
     index: (req,res) =>{ 
-        db.Operation.findAll({order: op.DESC, limit: 10})
+        db.Operation.findAll({ order: [["id","DESC"]], limit: 10})
         .then((result) =>{
             res.render('home',{
                 operations: result,
                 style: 'home'
             })
-        })    
+        })
+        .catch((err) => res.send(err));    
     }
 
 }
