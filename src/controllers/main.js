8b59d186207dec86dab892b1c5ba65f5cd50ef79
Denise Eichenblat <93154485+Denise-E@users.noreply.github.com>
2022-04-28 10:37:59 -0300

    ajustando controladores

--- src/controllers/main.js
+++ src/controllers/main.js
@@ -7,15 +7,13 @@ module.exports = {
         let lastTen = db.Operation.findAll({ order: [["id","DESC"]], limit: 10});
         let allOp = db.Operation.findAll();
         Promise.all([lastTen, allOp])
-        .then(([lastTen, allOp]) =>{
-           // res.send(allOp);
-                res.render('home',{
-                operations: lastTen,
-                all: allOp,
-                style: 'home'
+            .then(([lastTen, allOp]) =>{
+                    res.render('home',{
+                    operations: lastTen,
+                    all: allOp,
+                    style: 'home'
+                })
             })
-            })
-        .catch((err) => res.send(err));    
+            .catch((err) => res.send(err));    
     }
-
 }
--- src/controllers/operations.js
+++ src/controllers/operations.js
@@ -1,4 +1,3 @@
-const { where } = require("sequelize");
 const sequelize = require("sequelize");
 const db = require('../../database/models');
 
@@ -8,60 +7,61 @@ module.exports = {
     }),
     list: (req,res) => {
         db.Operation.findAll({order: [["id","DESC"]]})
-        .then(result => res.render('list', {
-            operations: result,
-            style: 'list'
-        }))
+            .then(result => res.render('list', {
+                operations: result,
+                style: 'list'
+            }))
+            .catch(err => res.send(err))
     },
     incomes: (req,res) => {
         db.Operation.findAll({where: {type: 'ingreso'}})
-        .then(result => res.render('incomes', { 
-            incomes: result,
-            style: 'incomes'
-        }))
+            .then(result => res.render('incomes', { 
+                incomes: result,
+                style: 'incomes'
+            }))
+            .catch(err => res.send(err))
     },
     outcomes: (req,res) => {
         db.Operation.findAll({where: {type: 'egreso'}})
-        .then(result => res.render('outcomes', { 
-            outcomes: result,
-            style: 'outcomes'
-        }))
+            .then(result => res.render('outcomes', { 
+                outcomes: result,
+                style: 'outcomes'
+            }))
+            .catch(err => res.send(err))
     },
     edit: (req,res) =>{
         db.Operation.findByPk(req.params.id)
-        .then((result) => res.render('edit',{
-            operation: result,
-            style: 'edit'
-        }))
-        .catch(err => res.send(err));
+            .then((result) => res.render('edit',{
+                operation: result,
+                style: 'edit'
+            }))
+            .catch(err => res.send(err));
     },
     show: (req,res) => {
         db.Operation.findByPk(req.params.id)
-        .then((result) =>res.render('show',{
-            operation: result,
-            style: 'show'
-        }))
-        .catch((err) => res.send(err));
+            .then((result) =>res.render('show',{
+                operation: result,
+                style: 'show'
+            }))
+            .catch((err) => res.send(err));
     },
     save: (req,res) => {
-      db.Operation.create({
+        db.Operation.create({
             type: req.body.operationType,
             description: req.body.description,
             value: parseInt(req.body.total),
             date: req.body.date
         })
-        .then(() => res.redirect("/")) //AJUSTAR DESP A VISTA DINÁMICA DE LA OPERACIÓN
+        .then(() => res.redirect("/"))
         .catch(err => res.send(err))
     },
     modify: (req, res) => {
-        db.Operation.update(
-            {
-                description: req.body.description,
-                value: parseInt(req.body.total),
-                date: req.body.date
-            },{where: {id: req.params.id}})
-        .then(() => res.redirect('/operations/' +req.params.id)) //AJUSTAR DESP A VISTA DINÁMICA DE LA OPERACIÓN
+        db.Operation.update({
+            description: req.body.description,
+            value: parseInt(req.body.total),
+            date: req.body.date
+        }, {where: {id: req.params.id}})
+        .then(() => res.redirect('/operations/' +req.params.id))
         .catch(err => res.send(err))
-    }
-     
+    }    
 }
